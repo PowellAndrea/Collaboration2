15084d9494acb2611f3ae5487dfea993dbf4f455
PowellAndrea <80601370+PowellAndrea@users.noreply.github.com>
2022-05-17 12:54:50 -0700

    first push

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collaboration
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ourClass test = new ourClass();
+			test.buildSpyStuff();
+			Console.WriteLine(ourClass.Encrypt("Goat"));
+
+		}
+	}
+}
---
+++ ourClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collaboration
+{
+	internal class ourClass
+	{
+
+		public Encoding asciiEncoding = Encoding.ASCII;
+
+		Dictionary<int,int> SpyStuff = new Dictionary<int,int>();
+
+		public void buildSpyStuff()
+		{
+			SpyStuff.Add(2, 9);
+		}
+
+		/* The encryption/decryption methods
+		 * must use some form of sum of all ASCII values
+		 * as a (partial) basis/start point for encrypting the string.
+		 */ 
+
+		public static string Encrypt(String unencrypted)
+		{
+			byte[] asciiValues = Encoding.ASCII.GetBytes(unencrypted);
+
+			string temp = asciiValues.ToString();
+
+			Console.WriteLine(temp);
+			Console.ReadLine();
+
+			return temp;
+		}
+
+	
+		public static string Decrypt( String encrypted)
+		{
+			string decrypted = string.Empty;
+			byte[] decryptedBytes = Encoding.ASCII.GetBytes(encrypted);	
+
+
+
+			return decrypted;
+		}
+	}
+}
